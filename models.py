bfb90c50548c54407d5ed015938951897fc6e709
Bashorun97 <bashorun.emma@gmail.com>
2020-05-22 11:56:10 +0100

    restored User's super function to previous state

--- models.py
+++ models.py
@@ -22,7 +22,7 @@ class User(db.Model):
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
         if self.role is None:
-            if self.email == 'admin@farmspeys.com':
+            if self.email == current_app.config['FLASKY_ADMIN']:
                 self.role = Role.query.filter_by(permissions=0xff).first()
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
